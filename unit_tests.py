937adac09493f1957ab3d1ce42a92a428c0e4d5d
netman <wana7248@colorado.edu>
2026-03-29 02:26:10 -0600

    fixing test definition case

--- unit_tests.py
+++ unit_tests.py
@@ -7,17 +7,17 @@ class JenkinsUnitTests(unittest.TestCase):
         with open('output.txt', 'r') as file:
             self.output = file.read()
     
-    def Test_R3_Lo99IP(self):
+    def test_R3_Lo99IP(self):
         for line in self.output:
             if "Router3" in line:
                 self.assertIn("10.1.3.1/24", line)
                 
-    def Test_R1_single_area(self):
+    def test_R1_single_area(self):
         for line in self.output:
             if "Router1" in line:
                 self.assertIn(" 0 ", line)
                 
-    def Test_ping_from_R2_to_R5(self):
+    def test_ping_from_R2_to_R5(self):
         r2 = {
             'device_type': 'ios',
             'host': '198.51.100.12',
